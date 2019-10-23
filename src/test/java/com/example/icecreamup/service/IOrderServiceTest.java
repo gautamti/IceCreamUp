f6b4373cf3ad770bd0a491aef5b491127b75570c
chaklahk <chaklahk@mail.uc.edu>
2019-10-22 21:20:13 -0400

    Added JUnit Tests
    Tests for OrderService and ProductService

---
+++ src/test/java/com/example/icecreamup/service/IOrderServiceTest.java
@@ -0,0 +1,36 @@
+package com.example.icecreamup.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.example.icecreamup.dto.OrderDTO;
+import com.example.icecreamup.services.IOrderService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class IOrderServiceTest
+{
+	@Autowired
+	private IOrderService OrderService;
+	
+	
+	@Test
+	public void testFetchOrder()
+	{
+		// Expected Value - Value to fetch
+		int orderId = 7;
+		OrderDTO order = OrderService.fetchById(orderId);
+		
+		// Actual Value - Value that was actually fetched
+		int actualOrderId = order.getOrderId();
+		
+		// Test that provided order id is the same as the actual order id.
+		assertEquals(orderId, actualOrderId);
+	}
+
+}
---
+++ src/test/java/com/example/icecreamup/service/IProductServiceTest.java
@@ -0,0 +1,37 @@
+package com.example.icecreamup.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.example.icecreamup.dto.OrderDTO;
+import com.example.icecreamup.dto.ProductDTO;
+import com.example.icecreamup.services.IProductService;
+import com.example.icecreamup.services.ProductServiceStub;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class IProductServiceTest
+{
+	@Autowired
+	private IProductService productService;
+	
+	@Test
+	public void test()
+	{
+		// Expected Value - Value to fetch
+		int productId = 7;
+		ProductDTO product = productService.fetchById(productId);
+		
+		// Actual Value - Value that was actually fetched
+		int actualProductId = product.getProductId();
+		
+		// Test that provided order id is the same as the actual order id.
+		assertEquals(productId, actualProductId);
+	}
+
+}
