0cef01bae2ae25d6e058df756a0f06a70eefb022
onegatg <onega.thomas@gmail.com>
2019-10-24 15:01:44 -0400

    Removed unused imports that caused warning messages in project

--- src/main/java/com/example/icecreamup/IcecreamupController.java
+++ src/main/java/com/example/icecreamup/IcecreamupController.java
@@ -4,12 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.example.icecreamup.dto.ProductDTO;
 import com.example.icecreamup.service.ITopOrderService;
-
-import org.springframework.web.bind.annotation.PostMapping;
 
 
 /**
