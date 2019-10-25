a450b6f579902ad3046c343414ba347ed7e57d23
Ryan Gigliotti <torresrs@mail.uc.edu>
2019-10-25 17:03:28 -0400

    Moved CSS to new files

--- src/main/java/com/example/icecreamup/IcecreamupController.java
+++ src/main/java/com/example/icecreamup/IcecreamupController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.example.icecreamup.dto.ProductDTO;
 import com.example.icecreamup.services.ITopOrderService;
